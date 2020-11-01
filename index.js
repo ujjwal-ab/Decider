29cb5cad035aa994ee5a06d6c0c9b796ef3267b1
ujjwal-ab <63387036+ujjwal-ab@users.noreply.github.com>
2020-11-01 20:36:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+var randomDiceImage="dice"+randomNumber1+".png";
+
+var randomImageSource="images/"+randomDiceImage;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomDiceImage="dice"+randomNumber2+".png";
+
+var randomImageSource="images/"+randomDiceImage;
+
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSource);
+
+if(randomNumber1>randomNumber2)
+	document.querySelector("h1").innerHTML="Ujjwal Wins"+" &#128525";
+else if(randomNumber2>randomNumber1)
+	document.querySelector("h1").innerHTML="Zikra Wins"+" &#128525";
+else
+	document.querySelector("h1").innerHTML="It's a Tie" +" &#128530";
