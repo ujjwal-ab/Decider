21b2b3ad24fb2b13444c5a7d5e198a07579ea5f6
ujjwal-ab <63387036+ujjwal-ab@users.noreply.github.com>
2020-11-02 17:41:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSource);
 
 if(randomNumber1>randomNumber2)
-	document.querySelector("h1").innerHTML="Ujjwal Wins"+" &#128525";
+	document.querySelector("h1").innerHTML="Player 1 Wins"+" &#128525";
 else if(randomNumber2>randomNumber1)
-	document.querySelector("h1").innerHTML="Zikra Wins"+" &#128525";
+	document.querySelector("h1").innerHTML="Player 2 Wins"+" &#128525";
 else
 	document.querySelector("h1").innerHTML="It's a Tie" +" &#128530";
